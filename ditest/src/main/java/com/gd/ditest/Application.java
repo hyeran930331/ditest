828ea7dfbcc2e10c821b24ccdf64eb779f6b8334
GDJ34 <GDJ34@DESKTOP-5G3759N>
2021-05-18 15:30:39 +0900

    4. 디펜드식 인터페이스 : 생성자를 만들지 않고 bean 을이용하여 객체간 의존도를 낮춤

--- ditest/src/main/java/com/gd/ditest/Application.java
+++ ditest/src/main/java/com/gd/ditest/Application.java
@@ -2,14 +2,18 @@ package com.gd.ditest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		// ApplicationContext ac -> Spring 2개의 빈 1)new Car(); 2)new Person();
 		System.out.println("test");
-		Person p = new Person();
+		//새로운걸 만들면 안되고 Person p = new Person(); 
+		//빈을 가져오기
+		ApplicationContext ac = SpringApplication.run(Application.class, args);
+		Person p = ac.getBean(Person.class); 
 		p.drive(); //왜 아직도 nullpoint
 	}
 
